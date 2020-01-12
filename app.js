f0db7548263f1bffee1d810890d07b6f28798db3
ViditVerma1915 <59385658+ViditVerma1915@users.noreply.github.com>
2020-01-12 22:51:52 +0530

    fff

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.get('/api', (req,res) => {
+res.json({
+    message: 'Welcome ------ :)'
+});
+});
+
+app.post('/api/posts',verifyToken,(req,res) => {
+    jwt.verify(req.token,'secretkey' , (err, authData) => {
+        if(err){
+            res.sendStatus(403);
+        } else {
+            res.json({
+                message: 'post created......',
+                authData
+            })
+        }
+    })
+    
+})
+
+app.post('/api/login', (req,res) => {
+    // Mock User
+    const user = {
+        id: 1,
+        username: 'vidit',
+        company: 'awc'
+    }
+
+    jwt.sign({user}, 'secretkey', { expiresIn: '30s' },(err,token) => {
+        res.json({
+            token
+        })
+    })
+})
+
+//Format of Token
+//Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req,res,next){
+// Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined'){
+        //Split at the space
+        const bearer = bearerHeader.split(' ');
+        //Get Token from array
+        const bearerToken = bearer[1];
+        //Set the token
+        req.token = bearerToken;
+        //Next middleware
+        next();
+    }
+    else {
+        // Forbidden
+        res.sendStatus(403);
+    }
+}
+
+app.listen(5000,() => console.log('Server Started on port 5000'));
